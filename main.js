65d04b9ebe27f043e7ff5a722398b5a4d6377def
PolMuri <109922379+PolMuri@users.noreply.github.com>
2022-08-04 18:08:02 +0200

    New features with more data in the web app

--- main.js
+++ main.js
@@ -5,26 +5,29 @@ const api = {
 }
 
 const card = document.getElementById('card');
-//hem de posar id a index html per poder obtenirlos aquí en javascript
-//per obtenir els id:
+//hem de posar id a index html per poder obtenirlos de l'html aquí en javascript
+//per obtenir els id (o podrien ser class):
 const city = document.getElementById('city');
 const date = document.getElementById('date');
 const tempImg = document.getElementById('temp-img');
 const temp = document.getElementById('temp');
 const weather = document.getElementById('weather');
 const range = document.getElementById('range');
+const humidity = document.getElementById('humidity');
+const wind = document.getElementById('wind');
+
 
 //li passem l'id de search form
 const searchform = document.getElementById('search-form');
 //volem obtenir l'informacio dels searchbox
 const searchbox = document.getElementById('searchbox');
-//a aquest form quan hi ha un event vamos a correr una funcio
+//a aquest form quan hi ha un event hi farem anar una funcio
 searchform.addEventListener('submit', onSubmit, true);
 
 //Creem la funcio de OnSubmit que rep un event
 function onSubmit(event) {
     //important el preventDefault perquè no faci una 
-    //redenderització de la p+àgina quan perdem el submit
+    //redenderització de la pàgina quan perdem el submit
     event.preventDefault();
     //posem la informació de dins el searchbox,és a dir, si escric barcelona i apreto, sortirà barcelona
     search(searchbox.value);
@@ -32,7 +35,13 @@ function onSubmit(event) {
 
 //basat en la temperatura, posem una imatge o una altra
 function updateImages(data){
-    const temp = toCelsius(data.main.temp);
+    const temp = (data.main.temp);
+    //variable que funciona a dins d'aquesta funció
+    //let us permet declarar variables limitant el vostre abast (scope) al bloc, declaració,
+    // o expressió on s'està usant.a diferència de la paraula clau var la qual defineix una
+    // variable global o local en una funció sense importar l'àmbit del bloc. L'altra 
+    //diferència entre var i let és que aquest darrer s'inicialitza a un valor només quan un 
+    //analitzador ho avalua
     let src='images/temp-mid.png';
     if(temp>=30){
       src= 'images/temp-high.png';
@@ -48,12 +57,13 @@ const clearName = () => {
 }
 
 //funció que ens permetrà treure la informació de la api
-async function search(query) {
+async function search(cityName) {
     try {
         //fetch per obtenir la informació
         //agafem la url per buscar, la q per buscar el pais o ciutat i a 
-        //part li passem la clau de l'api i el llenguatge que és espanyol
-        const response = await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
+        //part li passem la clau de l'api i el llenguatge que és espanyol, el fetch pot ser amb la url de la api només
+        const response = await fetch(`${api.url}?q=${cityName}&appid=${api.key}&lang=es&units=metric`);
+        
         //per obtenir la data
         const data = await response.json();
         //amb l'id card fem que no surti el card amb la info fins que no busquem
@@ -63,10 +73,13 @@ async function search(query) {
         //anem a remplaçar les dades que tenim per les que ens dona la api
         city.innerHTML = `${data.name}, ${data.sys.country}`;
         date.innerHTML = (new Date()).toLocaleDateString();
-        temp.innerHTML = `${toCelsius(data.main.temp)}c`;
-        //la primera lletra majúscula al tipus de temps
+        temp.innerHTML = `${(data.main.temp)}c`;
         weather.innerHTML = data.weather[0].description;
-        range.innerHTML = `${toCelsius(data.main.temp_min)}c / ${toCelsius(data.main.temp_max)}c`;
+        range.innerHTML = `Temp mín/máx: ${(data.main.temp_min)}c / ${(data.main.temp_max)}c`;
+        humidity.innerHTML = `Humitat: ${data.main.humidity}%`;
+        wind.innerHTML = `Velocitat vent: ${data.wind.speed} Met./seg.`;
+       
+       
         //basat en la temperatura, posem una imatge o una altra
         updateImages(data);
         //natejem el nom de la ciutat buscada amb aquesta funció
@@ -78,9 +91,11 @@ async function search(query) {
 }
 
 //convertim la temperatura a Celsius
-function toCelsius (kelvin) {
-    return Math.round(kelvin - 273.15);
-}
+//funció que utilitzava quan no sabia que les dades de l'API ja es podien cridar en Celsius
+//function toCelsius (kelvin) {
+    //return Math.round(kelvin - 273.15);
+//} 
+
 
 
 
